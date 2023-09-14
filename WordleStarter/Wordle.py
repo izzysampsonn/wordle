6c0c41f48083936ddf62b9f8848c50bb65f2a5ec
izzysampsonn <97313340+izzysampsonn@users.noreply.github.com>
2023-09-13 20:32:03 -0600

    Validate input

--- WordleStarter/Wordle.py
+++ WordleStarter/Wordle.py
@@ -6,32 +6,37 @@ from WordleDictionary import FIVE_LETTER_WORDS
 from WordleGraphics import WordleGWindow, N_COLS, N_ROWS
 
 def wordle():
-
-    def enter_action(s):
-        guessed_word = s.strip().lower()
-
-        if guessed_word == random_word:
-            gw.show_message("Hooray! You win.")
-        else:
-            gw.show_message("Nope. Try again.")
-
     gw = WordleGWindow()
-    gw.add_enter_listener(enter_action)
 
     # Randomly select a word from FIVE_LETTER_WORDS
     random_word = random.choice(FIVE_LETTER_WORDS)
     print(random_word)
+
+    def enter_action(s):
+        guessed_word = s.strip().lower()
+
+        # Check if it has 5 letters (IS)
+        if len(guessed_word) != 5:
+              gw.show_message("Invalid input: Must be 5 letters.")
+              return
+        
+        if guessed_word in FIVE_LETTER_WORDS: # See if it's a real word (IS)
+            if guessed_word == random_word:
+                    gw.show_message("Hooray! You win.")
+                    return
+            else:
+                    gw.show_message("Nope. Try again.")
+        else:
+             gw.show_message("Invalid input: Word not in dictionary.")
+
+
+    
+    gw.add_enter_listener(enter_action)
+
     
 # Startup code
 
 if __name__ == "__main__":
     wordle()
 
-
-    # Initialize total attempts variable IS
-    total_attempts = 0
-
-    # Allow player to enter 5 letter words IS
-    while total_attempts <= 6:
-        player_guess = input("Enter a 5  letter word: ")
         

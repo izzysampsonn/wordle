2361ac115f507d6d63f0fb26e431787fe69963ff
izzysampsonn <97313340+izzysampsonn@users.noreply.github.com>
2023-09-21 08:40:42 -0600

    Added colors - not working

--- WordleStarter/Wordle.py
+++ WordleStarter/Wordle.py
@@ -3,7 +3,7 @@
 import random
 
 from WordleDictionary import FIVE_LETTER_WORDS
-from WordleGraphics import WordleGWindow, N_COLS, N_ROWS
+from WordleGraphics import WordleGWindow, N_COLS, N_ROWS, CORRECT_COLOR, PRESENT_COLOR, MISSING_COLOR
 
 def wordle():
     gw = WordleGWindow()
@@ -25,12 +25,39 @@ def wordle():
                     gw.show_message("Hooray! You win.")
                     return
             else:
-                    gw.show_message("Nope. Try again.")
+                    # Addings stuff
+                correct_positions=[]
+                present_columns = []
+
+                # Check for correct letters in correct positions
+                for i in range(5):
+                    if guessed_word[i] == random_word[i]:
+                         correct_positions.append(i)
+
+                # Check correct letters in wrong positions
+                
+                for i in range(5):
+                    if i not in correct_positions and guessed_word[i] in random_word:
+                         present_columns.append(guessed_word.index(guessed_word[i]))
+
+                # Color the boxes accordingly
+                for row in range(N_ROWS):
+                     for col in range(N_COLS):
+                          letter = random_word[col]
+                          if col in correct_positions:
+                               gw.set_square_color(row, col, CORRECT_COLOR)
+                          elif col in present_columns:
+                               gw.set_square_color(row, col, PRESENT_COLOR)
+                          else:
+                               gw.set_square_color(row, col, MISSING_COLOR)
+                
+                # Move on to the next row
+                gw.set_current_row(gw.get_current_row() + 1)
+
+                gw.show_message("Nope. Try again.")
         else:
              gw.show_message("Invalid input: Word not in dictionary.")
 
-
-    
     gw.add_enter_listener(enter_action)
 
     
